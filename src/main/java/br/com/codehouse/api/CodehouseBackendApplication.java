e191d00601de2e8c2956b138776d9ceb1c08a2b9
unknown <lucask@gmail.com>
2020-09-08 15:03:35 -0300

    add new files

---
+++ src/main/java/br/com/codehouse/api/CodehouseBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.codehouse.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehouseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codehouse/api/CodehouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codehouse.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
