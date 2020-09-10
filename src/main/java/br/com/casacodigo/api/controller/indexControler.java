c86ecaec4439f7de0755814b83fa5947fc103b28
unknown <lucask@gmail.com>
2020-09-10 19:59:44 -0300

    config controller index bugfix#Swagger2

--- src/main/java/br/com/casacodigo/api/controller/indexControler.java
+++ src/main/java/br/com/casacodigo/api/controller/indexControler.java
@@ -1,10 +1,5 @@
 package br.com.casacodigo.api.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,8 +18,7 @@ public class indexControler {
 
     
     @GetMapping
-    public void  index(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-	String  path =  req.getContextPath()+"/swagger-ui.html";
-	resp.sendRedirect(path);
+    public String  index(){
+	return "configuration ok";
     }
 }
