4055051f93fb31260cd0e544502fcc8e3b7a9f88
unknown <lucask@gmail.com>
2020-09-10 21:06:33 -0300

    adicionando o SwaggerConfig

--- src/main/java/br/com/casacodigo/api/controller/indexControler.java
+++ src/main/java/br/com/casacodigo/api/controller/indexControler.java
@@ -1,8 +1,16 @@
 package br.com.casacodigo.api.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 
 /**
@@ -14,11 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Api(value = "index")
 public class indexControler {
 
     
-    @GetMapping
-    public String  index(){
-	return "configuration ok";
+    
+    @GetMapping  
+    @ApiOperation(httpMethod = "GET", value = "Redireciona para swagger-ui.html ")
+    public void   index(HttpServletRequest req, HttpServletResponse resp){
+	try {
+	    resp.sendRedirect(req.getContextPath() +"/swagger-ui.html");
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 }
--- src/main/java/br/com/casacodigo/api/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/casacodigo/api/swagger/SwaggerConfiguration.java
@@ -1,51 +1,45 @@
 package br.com.casacodigo.api.swagger;
 
-import java.util.function.Predicate;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
-@Configuration
-public class SwaggerConfiguration implements WebMvcConfigurer {
-    Predicate<String> apiUrl = PathSelectors.ant("/api/**");
-    Predicate<RequestHandler> basPackage = RequestHandlerSelectors.basePackage("br.com.codehouse.api");
-   
-    @Bean
-    public Docket api() {
-	 return new Docket( DocumentationType.SWAGGER_2)
-		 .select()
-		 .apis(basPackage).paths(apiUrl)
-		 .build().apiInfo(metaData());
-    }
-    
-    private ApiInfo metaData() {
-	    return new ApiInfoBuilder()
-	        .title("Code House API")
-	        .description(" API de livros baseados na casa do codigo.")
-	        .version("1.0.0")
-	        .license("Apache License Version 2.0")
-	        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
-	        .build();
-	  }
-    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry.addResourceHandler("swagger-ui.html")
-          .addResourceLocations("classpath:/META-INF/resources/");
+@Configuration @EnableSwagger2
+public class SwaggerConfiguration extends WebMvcConfigurationSupport{
+  
 
-      registry.addResourceHandler("/webjars/**")
-          .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    @Bean
+    public Docket greetingApi() {
+	return new Docket(DocumentationType.SWAGGER_2).select()
+		.apis(RequestHandlerSelectors
+		.basePackage("br.com.casacodigo.api"))
+		.build().apiInfo(metaData());
+
     }
+
+    private ApiInfo metaData() {
+	return new ApiInfoBuilder().title("Spring Boot REST API")
+		.description("Spring Boot REST API - Casa do cido com base")
+		.version("V1.0.0")
+		.license("Apache License Version 2.0")
+		.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+		.build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry.addResourceHandler("swagger-ui.html").
+	addResourceLocations("classpath:/META-INF/resources/");
+	registry.addResourceHandler("/webjars/**")
+	.addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+   
 }
